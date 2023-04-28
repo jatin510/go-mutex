a6c95cee29858880351c6f161313719d82483016
Jagdish Parihar <jatin6972@gmail.com>
2023-04-29 02:15:24 +0530

    go mutex

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type Player struct {
+	health int
+}
+
+func NewPlayer() *Player {
+	return &Player{
+		health: 100,
+	}
+}
+
+func startUILoop(p *Player) {
+	ticker := time.NewTicker(time.Second)
+
+	for {
+		fmt.Printf("player health: %d\r", p.health)
+		<-ticker.C
+	}
+}
+
+func startGameLoop(p *Player) {
+	ticker := time.NewTicker(time.Millisecond * 300)
+
+	for {
+		p.health -= rand.Intn(20)
+		if p.health <= 0 {
+			fmt.Println("GAME OVER")
+			break
+		}
+
+		<-ticker.C
+	}
+}
+
+func main() {
+	player := NewPlayer()
+
+	go startUILoop(player)
+	startGameLoop(player)
+}
